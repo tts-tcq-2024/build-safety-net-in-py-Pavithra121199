c09a9e05ec956e3bc7694367628406a9f7135d98
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-05 16:14:16 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -10,13 +10,13 @@ class TestSoundex(unittest.TestCase):
         self.assertEqual(generate_soundex("A"), "A000")
 
     def testcases(self):
-       # self.assertEqual(generate_soundex("Radhakrishnan"),"R362")  #Random name more than 4 characters
+        self.assertEqual(generate_soundex("Radhakrishnan"),"R326")  #Random name more than 4 characters
         self.assertEqual(generate_soundex("X"), "X000")            # Single character non-vowel
         self.assertEqual(generate_soundex("aeiou"),"A000")         # All vowels
         self.assertEqual(generate_soundex("Quite"), "Q300")        
         self.assertEqual(generate_soundex("Quiet"), "Q300")        # Similar sound 
         self.assertEqual(generate_soundex("A2B"), "A100")         # Non-alphabetic and pad with zeros
-        self.assertEqual(generate_soundex("BBCCDDEE"), "B123")     # Repeating characters with vowels ignored
+        self.assertEqual(generate_soundex("BCCDDEE"), "B230")     # Repeating characters with vowels ignored
         self.assertEqual(generate_soundex("HARRYPOTTER"), "H613")     # Example with same consecutive letters, containing 'y' which should be ignored   
         self.assertEqual(generate_soundex("HANUMAN"), "H555")     # Example with same consecutive code separated by a vowel - coded twice
         self.assertEqual(generate_soundex("RGYQBAF"), "R211")     # Example with same consecutive code separated by 'h', 'w' or 'y'  - coded as a single number  [ G and Q have the same code 2] [B and F have the same code 1]
